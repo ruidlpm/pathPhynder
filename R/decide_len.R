78e41719fc5a2b6bee24c4cc85a19176c9584201
ruidlpm <ruidlpm@gmail.com>
2018-10-07 18:19:49 +0100

    adding to tree

--- R/decide_len.R
+++ R/decide_len.R
@@ -60,6 +60,9 @@ max_tolerance<-args[3]
 
 samples<-names(br_sample_tables)
 
+best_nodes_table<-NULL
+
+
 for (samp in 1:length(br_sample_tables)){
 sample_name<-samples[samp]
 print(sample_name)
@@ -127,21 +130,16 @@ if (length(which(sums==max(sums)))>1){
 
 
 counts_for_best_path<-NULL
-# pos_score[[which(sapply(pos_score, sum)==max(sapply(pos_score, sum)))]]
-# neg_score[[which(sapply(pos_score, sum)==max(sapply(pos_score, sum)))]]
 counts_for_best_path<-countdata[match(best_path, countdata$Node2),]
 counts_for_best_path<-counts_for_best_path[!is.na(counts_for_best_path$Edge),]
 counts_for_best_path<-counts_for_best_path[!(counts_for_best_path$support==0 &  counts_for_best_path$notsupport==0),]
 counts_for_best_path_toplot<-counts_for_best_path[!(counts_for_best_path$support==0 &  counts_for_best_path$notsupport>0),]
-
-
 
 print(counts_for_best_path)
 
 
 write.table(counts_for_best_path, file=paste0(args[2],'/',sample_name,'.txt'),quote=F, row.names=F, sep="\t")
 
-print(best_path)
 
 if (sum(counts_for_best_path$support>0)==0){
     print("insufficient data")
@@ -157,9 +155,6 @@ best_edge<-counts_for_best_path$Edge[counts_for_best_path$support>0][sum(counts_
 best_node1<-counts_for_best_path$Node1[counts_for_best_path$support>0][sum(counts_for_best_path$support>0)]
 best_node2<-counts_for_best_path$Node2[counts_for_best_path$support>0][sum(counts_for_best_path$support>0)]
 
-print(best_edge)
-print(best_node1)
-print(best_node2)
 der_at_best<-counts_for_best_path$support[counts_for_best_path$Edge==best_edge]
 anc_at_best<-counts_for_best_path$notsupport[counts_for_best_path$Edge==best_edge]
 
@@ -241,9 +236,143 @@ if(pos_at_best_edge==0){
 # try(mtext(paste(anchgs$V1[grep(unlist(strsplit(sample_name, '\\.'))[1], make.names(anchgs$V1))],anchgs$V4[grep(unlist(strsplit(sample_name, '\\.'))[1], make.names(anchgs$V1))], sep="___")))
 # try(print(anchgs[grep(unlist(strsplit(sample_name, '\\.'))[1], make.names(anchgs$V1)),]))
 dev.off()
+
+
+
+
+best_nodes_table$sample_name<-as.character(best_nodes_table$sample_name)
+
+best_nodes_table<-rbind(best_nodes_table,data.frame(sample_name,best_node1, best_node2,pos_at_best_edge, total_len))
+
+
 closeAllConnections()
+}
+
+
+write.table(file=paste0(args[2],'/best_nodes_table.txt'),best_nodes_table, quote=F, row.names=F, sep='\t')
+
+pdf(file=paste0(args[2],'/best_node_location.pdf'), height=10, width=7)
+
+plot(tmptree, cex=0.3)
+
+for (i in 1:length(best_nodes_table$sample_name)){
+
+if(best_nodes_table$pos_at_best_edge[i]==0){
+    try(x <- getphylo_x(tmptree, best_nodes_table$best_node2[i]))
+    try(y <- getphylo_y(tmptree, best_nodes_table$best_node2[i]))
+    points(x,y,  col="black",bg=alpha("yellow", 0.3), pch=21, cex=1) 
+} else {
+    try(x <- getphylo_x(tmptree, best_nodes_table$best_node2[i]))
+    try(y <- getphylo_y(tmptree, best_nodes_table$best_node2[i]))
+    points(x-(best_nodes_table$total_len[i]-best_nodes_table$pos_at_best_edge[i]),y, col="black", bg=alpha("yellow", 0.3), pch=21, cex=1)
+}
+
 
 }
+dev.off()
+
+
+
+# require(phytools)
+
+# best_nodes_table<-read.table("results_folder/best_nodes_table.txt", h=T, stringsAsFactors=F)
+#  tmptree<-read.tree('/Users/rm890/testing/na_algo/revisiting_code/tree_sole_only.nwk')
+
+best_nodes_table<-best_nodes_table[order(best_nodes_table$pos_at_best_edge-best_nodes_table$total_len),]
+
+
+newtree<-tmptree
+newtree$edge.length<-tmptree$edge.length
+
+dup_entr<-best_nodes_table[c('best_node1' ,'best_node2' ,'pos_at_best_edge'  ,'total_len')][duplicated.data.frame(best_nodes_table[c('best_node1' ,'best_node2' ,'pos_at_best_edge'  ,'total_len')]),]
+
+dup_table<-merge(best_nodes_table, dup_entr)
+dup_table<-dup_table[order(dup_table$pos_at_best_edge-dup_table$total_len),]
+
+uniq_entr<-best_nodes_table[!best_nodes_table$sample_name %in%dup_table$sample_name ,]
+uniq_entr<-rbind(dup_table[dup_table$pos_at_best_edge==0,],uniq_entr)
+uniq_entr<-uniq_entr[order(uniq_entr$pos_at_best_edge-uniq_entr$total_len),]
+dup_table<-dup_table[dup_table$pos_at_best_edge!=0,]
+
+dup_table$pos_at_best_edge[which(duplicated(dup_table$pos_at_best_edge))]<-dup_table$pos_at_best_edge[which(duplicated(dup_table$pos_at_best_edge))]-0.0000000001
+uniq_entr<-rbind(dup_table,uniq_entr)
+uniq_entr<-uniq_entr[order(uniq_entr$pos_at_best_edge-uniq_entr$total_len),]
+
+
+
+
+for (i in 1:length(uniq_entr$best_node2)){
+    # Get descendants of final_node in tmptree
+    desc<-(getDescendants(tmptree, uniq_entr$best_node2[i]))
+    desc_tip_names<-tmptree$tip.label[tmptree$tip.label %in% tmptree$tip.label[desc]]
+
+    if(length(desc_tip_names)==1){
+        #if just 1 descendant, then final node is tip
+        final_node_in_new_tree<-which(newtree$tip.label==desc_tip_names)
+    } else if(length(desc_tip_names)>1){
+        #if more than 1 descendant, then need to find MRCA of all descendants
+        final_node_in_new_tree<-(findMRCA(newtree, desc_tip_names))
+    }
+    if (uniq_entr$pos_at_best_edge[i]==0){
+        (newtree<-bind.tip(tree=newtree, position = 0, edge.length=0.001, where=final_node_in_new_tree,tip.label = substring(as.character(uniq_entr$sample_name[i] ),1,9)))
+    } else if (uniq_entr$pos_at_best_edge[i]>0){
+        (newtree<-bind.tip(tree=newtree, position =(uniq_entr$total_len[i]-uniq_entr$pos_at_best_edge[i]), edge.length=abs(uniq_entr$pos_at_best_edge[i]-uniq_entr$total_len[i]), where=final_node_in_new_tree, tip.label = substring(as.character(uniq_entr$sample_name[i] ),1,9)))
+    }
+}
+
+# plot(ladderize(newtree), cex=0.5, tip.color = ifelse(newtree$tip.label %in% substring(best_nodes_table$sample_name,1,9), yes=2, no=1))
+
+
+# get_subtrees<-function(best_nodes_tables){
+#     #make subtrees with the samples that 1) belong to the same node; 2) have the same pos_at_best_edge
+#     #split by node
+#     node_split<-(split(best_nodes_tables,best_nodes_tables$best_node2))
+    
+#     subtree_list<-list()
+#     counter=0
+#     for (split_num in 1:length(node_split)){
+#        #get split by each node2
+#        tmp<-node_split[[split_num]]
+#        #split within node by pos_at_best_edge
+#         pos_split<-(split(tmp, tmp$pos_at_best_edge))
+#         if (length(pos_split)>1){
+#             dups<-which(sapply(pos_split, dim)[1,]>1)
+#             if (length(dups)>1){
+#                 for (dup_num in 1:length(dups)){
+#                     print(pos_split[dups][[dup_num]])
+#                     subtree_data<-pos_split[dups][[dup_num]]
+#                     subtree_len<-dim(subtree_data)[1]
+#                     subtree<-pbtree(n=subtree_len)
+#                     subtree$tip.label<-subtree_data$sample_name
+#                     subtree$edge.length<-subtree_data$total_len-subtree_data$pos_at_best_edge
+#                     counter=counter+1
+#                     subtree_list[[counter]]<-subtree
+#                 }
+#             }
+#         }
+#     }
+#     return(subtree_list)
+# }
+# 
+# all_subtrees<-get_subtrees(dup_table)
+
+
+
+
+
+
+
+write.tree(newtree,file=paste0(args[2],'/added_anc_best_node_location.nwk'))
+
+pdf(file=paste0(args[2],'/added_anc_best_node_location.pdf'), height=10, width=7)
+plot(ladderize(newtree), cex=0.5, tip.color = ifelse(newtree$tip.label %in% substring(best_nodes_table$sample_name,1,9), yes=2, no=1))
+dev.off()
+
+
+
+
+
+
 
 
 # pdf(file="test.pdf")
