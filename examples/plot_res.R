fb6a33ba038a9f86aa1bbda1e84828d7f56633f9
ruidlpm <ruidlpm@gmail.com>
2018-11-08 09:50:27 +0000

    added example

---
+++ examples/plot_res.R
@@ -0,0 +1,13 @@
+require("phytools")
+tree<-read.tree("complete_tree.nwk")
+
+pdf(file="complete_example_tree.pdf")
+res<-read.table("tree_data/complete_example.edge_df.txt", h=T, sep="\t")
+res<-res[!is.na(res$positions),]
+plot(tree, direction="down", edge.color="grey",  edge.width=3, tip.color="darkgrey")
+nodelabels(node=res$Node2, frame='none', pch=15, col="steelblue")
+nodelabels(node=res$Node2, pos=4, frame='none', text=res$Node2, col="steelblue")
+edgelabels(edge=res$Edge, frame='none', pch=20, col="indianred")
+edgelabels(edge=res$Edge, frame='none',text=res$Edge, pos=2, col="indianred")
+dev.off()
+
---
+++ examples/run_example.sh
@@ -0,0 +1,1 @@
+Rscript ~/in_development/pathPhynder/R/assign_noNA.R complete_tree.nwk  complete_example.vcf complete_example
