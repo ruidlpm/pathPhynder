1bdb202fa64cfabec21054fcaca869c423788a0b
ruidlpm <ruidlpm@gmail.com>
2019-02-24 16:29:44 +0000

    big change. Now everything runs at the sample level

--- R/pileup_and_filter.R
+++ R/pileup_and_filter.R
@@ -1,32 +1,24 @@
 # pileup_and_filter.R
 # runs pileup and filters bases, outputs intree files
 
-
-
-
-
 args = commandArgs(trailingOnly=TRUE)
 
 # test if no args are given
-if (length(args)!=7) {
+if (length(args)!=8) {
     stop("  Arguments needed.\n
 	\tusage
-	\tRscript pileup_and_filter.R <bam_list> <data_prefix> <folder_out> <refgen_path> <mode>['conservative'/'relaxed'] <chromosome_name>>['chrY'/'Y'] <pileup_read_mismatch_threshold>[default 0.7]
+	\tRscript pileup_and_filter.R <bam_list> <data_prefix> <folder_out> <refgen_path> <mode>['conservative'/'relaxed'] <chromosome_name>>['chrY'/'Y'] <pileup_read_mismatch_threshold>[default 0.7] bam file or list of bams
     	", call.=FALSE)
 }
-
-# } else {
-    # cat("Command used:",'\n\n')
-    # cat(paste("pileup_and_filter.R", args[1], args[2],args[3]),args[4],args[5],args[6],args[7], '\n\n')
-# }
 
 
 sites_data<-args[2] 
 intree_folder<-args[3]
+refgen_path<-args[4]
 mode<-args[5]
 chromosome_name<-args[6]
 pileup_read_mismatch_threshold<-args[7]
-
+input_type<-args[8]
 
 dir.create(intree_folder, showWarnings = FALSE)
 
@@ -45,49 +37,32 @@ for (testfile in c(args[1], sites_var, args[4])){
 	}
 }
 
-bam_list<-read.table(args[1], stringsAsFactors=F)
-refgen_path<-args[4]
 
+if (input_type=="bam_file"){
+		file_path<-args[1]
+		sample_name<-unlist(strsplit(file_path,'\\/'))[as.numeric(length(unlist(strsplit(file_path,'\\/'))))]
 
-
-
-
-
-for(samp in 1:length(bam_list$V1)){
-	file_path<-bam_list$V1[samp]
-	if (dim(bam_list)[2]>1){
-		sample_name<-bam_list$V2[samp]
-	} else if (dim(bam_list)[2]==1){
-		sample_name<-unlist(strsplit(bam_list$V1[samp],'\\/'))[as.numeric(length(unlist(strsplit(bam_list$V1[samp],'\\/'))))]
-	}
-	
-	if (!file.exists(file_path)) {
-		print(paste(file_path, "- does this file exist?"))
-	} else {
-
+		if (!file.exists(file_path)) {
+			print(paste(file_path, "- does this file exist?"))
+		} else {
+			pos_var<-paste0(sites_data,".sites.txt")
+			
+			cmd=paste0("samtools mpileup ",
+				paste0(file_path),
+				" --ignore-RG --positions ",
+				sites_var,
+				paste0(" -f ",refgen_path),
+				paste0(">", intree_folder,'/',sample_name,".pileup"))
+			
+			system(cmd, wait=T)
 		
-		pos_var<-paste0(sites_data,".sites.txt")
-		
-		cmd=paste0("samtools mpileup ",
-			paste0(file_path),
-			" --ignore-RG --positions ",
-			sites_var,
-			paste0(" -f ",refgen_path),
-			paste0(">", intree_folder,'/',sample_name,".pileup"))
-		
-		system(cmd, wait=T)
-	
-		
-		cmd2=paste0("python3 ~/in_development/pathPhynder/inst/python/call_bases_chrY_v2.1.py ",
-			paste0(" -i ", intree_folder,'/',sample_name,".pileup"),
-			paste0(" -m ", mode),
-			paste0(" -o ", intree_folder,'/',sample_name,".intree.txt"),
-			paste0(" -t ", pos_var),
-			paste0(" -c ", pileup_read_mismatch_threshold)
-			)
-
-		
-		
-		system(cmd2, wait=T)
-	}	
-}
+			cmd2=paste0("python3 ~/in_development/pathPhynder/inst/python/call_bases_chrY_v2.1.py ",
+				paste0(" -i ", intree_folder,'/',sample_name,".pileup"),
+				paste0(" -m ", mode),
+				paste0(" -o ", intree_folder,'/',sample_name,".intree.txt"),
+				paste0(" -t ", pos_var),
+				paste0(" -c ", pileup_read_mismatch_threshold)
+				)
+			system(cmd2, wait=T)
+		}	
+}	
--- pathPhynder.R
+++ pathPhynder.R
@@ -2,15 +2,16 @@
 # Author: Rui Martiniano
 # Contact: rm890 [at] cam.ac.uk
 
-suppressPackageStartupMessages(library("optparse"))
+suppressWarnings(suppressPackageStartupMessages(library("optparse")))
+suppressWarnings(suppressPackageStartupMessages(library("phytools")))
 
 
 option_list <- list(
     make_option(c("-s", "--step"), default="all", help="Specifies which step to run. Options:
     \t\t\t- all - runs all steps of the analysis.
     \t\t\t- 1 or pileup_and_filter - runs pileup in ancient bam files and filters bases.
-    \t\t\t- 2 or ancient_SNPs_to_branches - adds anc/der alleles to the input tree.
-    \t\t\t- 3 or decide - finds the best branch/node of the tree for each sample.
+    \t\t\t- 2 or chooseBestPath - finds the best branch/node of the tree for each sample.
+    \t\t\t- 3 or addToTree - adds ancients samples to tree.
     \t\t\t[default %default]"),
 
     make_option(c("-i","--input_tree"),
@@ -21,10 +22,13 @@ option_list <- list(
         help = "Prefix for the data files associated with the tree.
         \tThese were previously generated in the branch assignment step. [required]"),
 
-    make_option(c("-b","--bam_list"), 
+    make_option(c("-b","--bam_file"), 
+        help = "Input bam file. [required]"),
+
+    make_option(c("-l","--list_of_bam_files"), 
         help = "List of paths to bam files. [required]"),
 
-    make_option(c("-r","--reference"), default="data/reference_sequences/hs37d5_Y.fa.gz", 
+    make_option(c("-r","--reference"), default="~/in_development/pathPhynder/data/reference_sequences/hs37d5_Y.fa.gz", 
         help = "Reference genome (fasta format). [default \"%default\"]"),
 
     make_option(c("-m","--mode"), default="conservative", 
@@ -37,16 +41,92 @@ option_list <- list(
     make_option(c("-c", "--pileup_read_mismatch_threshold"), type="numeric", default=0.7, 
         help = "Mismatch threshold for accepting a variant (for cases where reads for both alleles are present in pileup).
         \tFor a variant to pass filtering, reads containing the most frequent allele have to occur at least  
-        \tat x proportion of the total reads. 1 is the most stringent, 0.5 is the most relaxed. [default %default]")
+        \tat x proportion of the total reads. 1 is the most stringent, 0.5 is the most relaxed. [default %default]"),
+
+    make_option(c("-o", "--output_prefix"), type="character", default="test", 
+        help = "prefix for output, for example, sample name [default %default]")
 
 )
-
 
 
 packpwd<-("~/in_development/pathPhynder/R")
 
 # get command line options, if help option encountered print help and exit,
 opt <- parse_args(OptionParser(option_list=option_list))
+
+
+# print(opt)
+
+#test if arguments were given
+if (is.null(read.tree(opt$input_tree))){
+    stop("Please provide valid tree Newick file (-i).")
+} else if (is.null(opt$input_tree)){
+    stop("Please provide valid tree Newick file (-i).")
+} else if (is.null(opt$bam_file) & is.null(opt$list_of_bam_files)){
+    stop("Please provide either a bam file (-b) or a list of bam files (-l).")
+} else if (length(opt$bam_file)>0 & length(opt$list_of_bam_files)>0){
+    stop("Please provide either a bam file (-b) or a list of bam files (-l), not both.")
+} else if (is.null(opt$prefix)){
+    stop("Please provide a tree data prefix.")
+}
+
+
+#test if bam files are bams
+checkBamIntegrity<-function(bam_file){
+    #test if bam files are bams
+    if (file_test("-f", bam_file)==F){
+        stop(paste0(bam_file," bam file does not exist."))
+    } else {
+        magic = readChar(gzfile(bam_file, 'r'), 4)
+        if (!identical(magic, 'BAM\1')){
+            stop(paste0(bam_file," is not a valid bam file."))
+        }
+    }
+}
+
+
+checkBamListIntegrity<-function(list_bams){
+    if (file_test("-f", list_bams)==F){
+        stop(paste0(list_bams," bam file list does not exist."))
+    } else {
+        bam_file_list<-read.table(list_bams)
+        for (bam in bam_file_list$V1){
+        checkBamIntegrity(bam) 
+        }
+    }
+}
+
+
+#############
+#test if files exist
+#############
+
+#decide on the type of input
+if (is.null(opt$list_of_bam_files)){
+    if (file_test("-f", opt$bam_file)==F){
+        stop("Please provide an existing bam file")
+    } else {
+        checkBamIntegrity(opt$bam_file)
+    }
+    input_type="bam_file"
+}
+
+if (is.null(opt$bam_file)){
+    if (file_test("-f", opt$list_of_bam_files)==F){
+        stop("Please provide an existing list of bam files")
+    } else {
+        checkBamListIntegrity(opt$list_of_bam_files)
+    }
+    input_type="bam_list"
+}
+
+if (file_test("-f", opt$reference)==F){
+    stop("Please provide an existing reference genome fasta file.")
+} else if (file_test("-f", paste0(opt$prefix,'.sites.bed'))==F){
+    stop("Please provide a prefix for existing tree data.")
+} 
+
+
 
 
 #decide whether to use "chrY" or "Y"
@@ -59,42 +139,23 @@ if (length(grep("hg19", opt$reference))>0){
 }
 
 
+
+
+#print parameters into terminal
 cat("\n\tpathPhynder v.0.0 \n", "\n\tParameters:\n")
 cat("\n\t--input_tree ", opt$input_tree)
 cat("\n\t--prefix ", opt$prefix)
-cat("\n\t--bam_list ", opt$bam_list)
+
+if (input_type=="bam_file"){
+    cat("\n\t--bam_file ", opt$bam_file)
+} else if (input_type=="bam_list"){
+    cat("\n\t--list_of_bam_files ", opt$list_of_bam_files)
+}
+
 cat("\n\t--reference ", opt$reference)
 cat("\n\t--mode ", opt$mode)
 cat("\n\t--maximumTolerance ", opt$maximumTolerance)
 cat("\n\t--pileup_read_mismatch_threshold ", opt$pileup_read_mismatch_threshold,'\n\n')
-
-
-
-
-#test if files exist
-if (file_test("-f", opt$input_tree)==F){
-    stop("Please provide an existing tree Newick file.")
-} else if (file_test("-f", opt$bam_list)==F){
-    stop("Please provide an existing bam list.")
-} else if (file_test("-f", opt$reference)==F){
-    stop("Please provide an existing reference genome fasta file.")
-} else if (file_test("-f", paste0(opt$prefix,'.sites.bed'))==F){
-    stop("Please provide a prefix for existing tree data.")
-} 
-
-
-#test if bam files are bams
-bam_list<-read.table(opt$bam_list)
-for (bam in bam_list$V1){
-    if (file_test("-f", bam)==F){
-        stop(paste0(bam," bam file does not exist. Fix the sample list."))
-    } else {
-        magic = readChar(gzfile(bam, 'r'), 4)
-        if (!identical(magic, 'BAM\1')){
-            stop(paste0(bam," is not a valid bam file."))
-        }
-    }
-}
 
 
 
@@ -104,28 +165,87 @@ if( opt$step == "all") {
     
     cat("All steps.\n")
 
-    system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),opt$bam_list,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold ))
+    # if (input_type=="bam_file"){
+    #     cat("\n\t--bam_file ", opt$bam_file)
+    # } else if (input_type=="bam_list"){
+    #     cat("\n\t--list_of_bam_files ", opt$list_of_bam_files)
+    # }
 
-    system(paste("Rscript", paste0(packpwd,"/ancient_SNPs_to_branches.R"),opt$input_tree,opt$prefix,'intree_folder', 'results_folder'))
+    if (input_type=="bam_file"){
 
-    system(paste("Rscript", paste0(packpwd,"/decide_len.R"),opt$input_tree, 'results_folder', opt$maximumTolerance, opt$prefix))
+        system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),opt$bam_file,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold, 'bam_file' ))
+
+        system(paste("Rscript", paste0(packpwd,"/chooseBestPath.R"),opt$input_tree,opt$prefix,paste0('intree_folder/',opt$bam_file,'.intree.txt'), 'results_folder',  opt$maximumTolerance))
+
+    } else if (input_type=="bam_list"){
+
+        bam_list<-read.table(opt$list_of_bam_files, stringsAsFactors=F)
+
+        for(samp in bam_list$V1){
+            system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),samp,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold, 'bam_file' ))
+            system(paste("Rscript", paste0(packpwd,"/chooseBestPath.R"),opt$input_tree,opt$prefix,paste0('intree_folder/',samp,'.intree.txt'), 'results_folder',  opt$maximumTolerance))
+
+        }
+    }
+
+
+        # \tRscript chooseBestPath.R <input_phylogeny.nwk> <prefix> intree.txt <results_folder> <maxThreshold>
+
+
+
+
+
+
+    # system(paste("Rscript", paste0(packpwd,"/ancient_SNPs_to_branches.R"),opt$input_tree,opt$prefix,'intree_folder', 'results_folder'))
+
+    # system(paste("Rscript", paste0(packpwd,"/decide_len.R"),opt$input_tree, 'results_folder', opt$maximumTolerance, opt$prefix))
 
 } else if(opt$step == "pileup_and_filter" | opt$step == 1) {
 
     cat("Running pileup_and_filter\n\n")
-    system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),opt$bam_list,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold))
 
-} else if(opt$step == "ancient_SNPs_to_branches" | opt$step == 2) {
 
-    cat("Running ancient_SNPs_to_branches\n")
-    system(paste("Rscript", paste0(packpwd,"/ancient_SNPs_to_branches.R"),opt$input_tree,opt$prefix,'intree_folder', 'results_folder'))
+    if (input_type=="bam_file"){
+        system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),opt$bam_file,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold, 'bam_file' ))
+    } else if (input_type=="bam_list"){
+        bam_list<-read.table(opt$list_of_bam_files, stringsAsFactors=F)
 
-} else if(opt$step == "decide" | opt$step == 3) {
+        for(samp in bam_list$V1){
+            system(paste("Rscript", paste0(packpwd,"/pileup_and_filter.R"),samp,opt$prefix,'intree_folder', opt$reference, opt$mode, chromosome_name,opt$pileup_read_mismatch_threshold, 'bam_file' ))
+        }
+    }
 
-    cat("Running decide\n")
-    system(paste("Rscript", paste0(packpwd,"/decide_len.R"),opt$input_tree, 'results_folder', opt$maximumTolerance, opt$prefix))
+} else if(opt$step == "chooseBestPath" | opt$step == 2) {
 
-} 
+    cat("Running chooseBestPath\n")
+
+    if (input_type=="bam_file"){
+        system(paste("Rscript", paste0(packpwd,"/chooseBestPath.R"),opt$input_tree,opt$prefix,paste0('intree_folder/',opt$bam_file,'.intree.txt'), 'results_folder',  opt$maximumTolerance))
+    } else if (input_type=="bam_list"){
+        bam_list<-read.table(opt$list_of_bam_files, stringsAsFactors=F)
+
+        for(samp in bam_list$V1){
+            system(paste("Rscript", paste0(packpwd,"/chooseBestPath.R"),opt$input_tree,opt$prefix,paste0('intree_folder/',samp,'.intree.txt'), 'results_folder',  opt$maximumTolerance))
+        }
+    }
+
+
+} else {
+    stop("Choose the step you would like to run.
+        Options:
+        \t- all - runs all steps of the analysis.
+        \t- 1 or pileup_and_filter - runs pileup in ancient bam files and filters bases.
+        \t- 2 or chooseBestPath - finds the best branch/node of the tree for each sample.
+        \t- 3 or addToTree - adds ancients samples to tree.")
+}
+# else if(opt$step == "addToTree" | opt$step == 2) {
+
+#     cat("Running addToTree\n")
+#     system(paste("Rscript", paste0(packpwd,"/addToTree.R"),opt$input_tree,opt$prefix,'intree_folder', 'results_folder'))
+
+# }
+
+
 
 cat("\n")
 
