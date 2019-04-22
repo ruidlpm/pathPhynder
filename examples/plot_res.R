6eff71dd608ae3c6028ac9a57043ded49e05d122
ruidlpm <ruidlpm@gmail.com>
2019-04-22 17:50:33 +0100

    fixed plotting code example

--- examples/plot_res.R
+++ examples/plot_res.R
@@ -1,8 +1,8 @@
 require("phytools")
-tree<-read.tree("small_example_tree")
+tree<-read.tree("small_example_tree.nwk")
 
 pdf(file="small_example_tree.pdf")
-res<-read.table("tree_data/small_example_tree.edge_df.txt", h=T, sep="\t")
+res<-read.table("tree_data/small_example.edge_df.txt", h=T, sep="\t")
 res<-res[!is.na(res$positions),]
 plot(tree, direction="down", edge.color="grey",  edge.width=3, tip.color="darkgrey")
 nodelabels(node=res$Node2, frame='none', pch=15, col="steelblue")
